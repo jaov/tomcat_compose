513fb169ea2d4a36ca10d2d86f6ed50fda17f90f
jesuso <j.ochoa@norteconecta.net>
2025-12-26 17:11:46 -0400

    added backup filename to environment options

--- postgres-init/restore.sh
+++ postgres-init/restore.sh
@@ -4,6 +4,6 @@ set -e # Stop immediately if a command fails
 echo "Starting Restore Process..."
 
 # Use the variables already defined in your docker-compose environment
-pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/my_db.backup
+pg_restore -v -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/my_db.backup
 
 echo "Restore Complete!"
