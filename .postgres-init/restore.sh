b95701d5edc5f08a645e6704be9304c21e659c0f
jesuso <j.ochoa@norteconecta.net>
2025-12-26 16:45:38 -0400

    added postgres restore

---
+++ .postgres-init/restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e # Stop immediately if a command fails
+
+echo "Starting Restore Process..."
+
+# Use the variables already defined in your docker-compose environment
+pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/my_db.backup
+
+echo "Restore Complete!"
